ba77bf59e2dc5b725b063733354966d61673eae8
felixbo86 <tbdream@126.com>
2020-03-23 12:22:09 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,40 +8,41 @@ module.exports = {
         // 作为特例，默认语言可以使用 '/' 作为其路径。
         '/': {
           lang: 'en-US', // 将会被设置为 <html> 的 lang 属性
-          title: 'nothing',
+          title: 'thirdCloudDevice',
           description: 'Vue-powered nothing Site Generator'
         },
         '/zh/': {
           lang: 'zh-CN',
-          title: '云云对接说明',
-          description: 'Vue 云云对接过程描述及注意事项'
+          title: '三方云设备接入',
+          description: '云云对接过程描述及注意事项'
         }
       },
 	  
-	  //gitub地址配置
-	    themeConfig: {
-			// 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-			repo: 'haier-iot/thirdCloudDevice',
-			// 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
-			// "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
-			repoLabel: '查看源码',
 
-			// 以下为可选的编辑链接选项
+    //多语言皮肤配置
+     themeConfig: {
+        logo: '/assets/img/logo.png',
 
-			// 假如你的文档仓库和项目本身不在一个仓库：
-			docsRepo: 'haier-iot/thirdCloudDevice',
-			// 假如文档不是放在仓库的根目录下：
-			docsDir: 'docs',
-			// 假如文档放在一个特定的分支下：
-			docsBranch: 'master',
-			// 默认是 false, 设置为 true 来启用
-			editLinks: true,
-			// 默认为 "Edit this page"
-			editLinkText: '帮助我们改善此页面！'
- 
-		},
-      //多语言皮肤配置
-      themeConfig: {
+        // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
+        repo: 'haier-iot/thirdCloudDevice',
+        // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
+        // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+        repoLabel: '查看源码',
+
+        // 以下为可选的编辑链接选项
+
+        // 假如你的文档仓库和项目本身不在一个仓库：
+        docsRepo: 'haier-iot/thirdCloudDevice',
+        // 假如文档不是放在仓库的根目录下：
+        docsDir: 'docs',
+        // 假如文档放在一个特定的分支下：
+        docsBranch: 'master',
+        // 默认是 false, 设置为 true 来启用
+        editLinks: true,
+        // 默认为 "Edit this page"
+        editLinkText: '帮助我们改善此页面！',
+
+
         locales: {
           '/': {
             selectText: 'Languages',
@@ -55,8 +56,10 @@ module.exports = {
               }
             },
             algolia: {},
+             // 定义导航栏
             nav: [
-              { text: 'Nested', link: '/nested/', ariaLabel: 'Nested' }
+              { text: 'Home', link: '/' },
+              { text: 'Nested', link: '/nested/', ariaLabel: 'Nested' },
             ],
             sidebar: {
               '/': [/* ... */],
@@ -79,10 +82,47 @@ module.exports = {
             },
             // 当前 locale 的 algolia docsearch 选项
             algolia: {},
+            // 定义导航栏
             nav: [
-              { text: '嵌套', link: '/zh/nested/' }
+              { text: '首页', link: '/' },
+              { text: '官方平台',  ariaLabel: '官方平台介绍',
+                  items: [
+                    { text: '海尔智家', link: 'https://www.haier.com/smarthome/' },
+                    { text: '海尔优家', link: 'http://www.haieruplus.com' },
+                    { text: '海极网', link: 'http://www.haigeek.com' },
+                  ]
+              },
+              { 
+                text: '平台文档',
+                ariaLabel: '其他平台文档',
+                items: [
+                  { text: '设备接入', 
+                  items: [
+                    { text: '概况', link: 'https://haier-iot.github.io/userGuide/' },
+                    { text: '直连设备', link: 'https://haier-iot.github.io/userGuide/#/zh-cn/DeviceGuide/directDevice/overView' },
+                    { text: '类直连设备', link: 'https://haier-iot.github.io/userGuide/#/zh-cn/DeviceGuide/CloudDevice/overView' },
+                    { text: '云云对接', link: 'https://haier-iot.github.io/userGuide/#/zh-cn/DeviceGuide/Cloud2Cloud/overView' },
+                ]
+                },
+                  { text: 'IOT服务', 
+                    items: [
+                      { text: '用户中心', link: 'https://haier-iot.github.io/doc/#/zh-cn/AccountManage' },
+                      { text: '设备中心', link: 'https://haier-iot.github.io/doc/#/zh-cn/DevicesManage' },
+                      { text: '家庭中心', link: 'https://haier-iot.github.io/doc/#/zh-cn/FamilyManage' },
+                      { text: '消息中心', link: 'https://haier-iot.github.io/doc/#/zh-cn/MessageManage' },
+                      { text: '场景中心', link: 'https://haier-iot.github.io/doc/#/zh-cn/IFTTTManage' },
+                    ]
+                }
+                ]
+               },
+              { text: '服务文档', link: 'https://haier-iot.github.io/doc/' },
             ],
+
+            //侧边栏
             sidebar: {
+              sidebarDepth: 1,    // 可选的, 默认值是 1
+              displayAllHeaders: false, // 默认值：false 显示所有页面的标题链接
+              activeHeaderLinks: true, // 默认值：true 活动的标题链接
               '/zh/': [/* ... */],
               '/zh/nested/': [/* ... */]
             }
